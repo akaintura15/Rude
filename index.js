8d4df9bcce1b518e588ce5908c0137849e1b3c55
akaintura15 <48716616+akaintura15@users.noreply.github.com>
2024-01-28 20:28:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import{getDatabase,ref,push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import{getDatabase,ref,push,onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 const appSettings = {
@@ -9,8 +9,29 @@ const appSettings = {
 const app=initializeApp(appSettings)
 const database=getDatabase(app)
 const data = ref(database,"rude")
+
+
 const inputa = document.getElementById("input-field")
 const adda=document.getElementById("add-button")
+const booksEl = document.getElementById("books")
+
+
+onValue(data, (snapshot) => {
+    const dataVal = snapshot.val();
+    console.log("Data from the database:", dataVal);
+    let booksArray = Object.values(snapshot.val())
+    
+    // Challenge: Write a for loop where you console log each book.
+    for (let i = 0; i < booksArray.length; i++) {
+        let currentBook = booksArray[i]
+        
+        
+        appendBookToBooksListEl(currentBook)
+    }
+    inputa.value = "";// Clear the input field after sending the message
+    scrollToBottom();
+})
+
 
 adda.addEventListener("click", function(){
     
@@ -18,4 +39,16 @@ adda.addEventListener("click", function(){
     push(data,inputvalue)
 
     console.log(`${inputa.value} added to database`)
+    
+  
+  
 })  
+
+function appendBookToBooksListEl(bookValue) {
+    booksEl.innerHTML += `<li>${bookValue}</li>`
+    
+}
+
+function scrollToBottom() {
+    booksEl.scrollTop = booksEl.scrollHeight;
+}
